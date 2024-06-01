2e562119d365c354182b4246a37db5b425c67752
Bruno Almeida <129557614+MysticXiz@users.noreply.github.com>
2024-05-31 22:23:07 -0300

    Add files via upload

---
+++ script/elementos.js
@@ -0,0 +1,63 @@
+function criarHeader(){
+    const header = document.createElement('header');
+    const linkContainer = document.createElement('nav');
+    linkContainer.className = 'linkContainer'
+
+    const headerBotao = document.createElement('a');
+    headerBotao.href = '/javascripit/index.html'
+    headerBotao.className = 'headerBotao home'
+    headerBotao.innerHTML = 'Home'
+    const headerBotao2 = document.createElement('a');
+    headerBotao2.href = '/javascripit/paginas/galeria.html'
+    headerBotao2.className = 'headerBotao galeria'
+    headerBotao2.innerHTML = 'Galeria'
+    const headerBotao3 = document.createElement('a');
+    headerBotao3.href = '/javascripit/paginas/cadastro.html'
+    headerBotao3.className = 'headerBotao cadastro'
+    headerBotao3.innerHTML = 'Cadastre-se'
+    const botaoAltModoescuro = document.createElement('button');
+    botaoAltModoescuro.className = 'botaoAltModoescuro'
+    botaoAltModoescuro.innerHTML = 'Modo escuro'
+    botaoAltModoescuro,addEventListener('click', ()=> alternarModoEscuro())
+
+
+    linkContainer.appendChild(headerBotao);
+    linkContainer.appendChild(headerBotao2);
+    linkContainer.appendChild(headerBotao3);
+
+    header.appendChild(linkContainer);
+    header.appendChild(botaoAltModoescuro);
+    document.body.insertBefore(header, document.body.firstChild);
+    const paginaAtual = window.location.pathname.split('/').pop(); 
+    setTimeout(() => {
+    if (paginaAtual === 'index.html') {
+        headerBotao.classList.add('botaoSelecionado');
+    } else if (paginaAtual === 'galeria.html' || paginaAtual === 'galeriacompleta.html' ) {
+        headerBotao2.classList.add('botaoSelecionado');
+    } else if (paginaAtual === 'cadastro.html') {
+        headerBotao3.classList.add('botaoSelecionado');
+    }
+    document.querySelector('.secaoConteudo').classList.add('transicao')
+    
+    }, 1);
+    
+}
+function criarFooter(){
+    
+}
+criarHeader()
+function alternarModoEscuro() {
+    const elementos = document.body;
+    const modoAtual = elementos.style.getPropertyValue('--background-padrao-cor');
+    const novoModo = modoAtual === '#ffffff' ? '#151515' : '#ffffff';
+  
+    elementos.style.setProperty('--background-padrao-cor', novoModo);
+    localStorage.setItem('modoEscuro', novoModo === '#151515');
+  }
+  
+  function carregarConfiguracoes() {
+    const modoEscuro = localStorage.getItem('modoEscuro') === 'true';
+    if (modoEscuro) {
+      alternarModoEscuro();
+    }
+  }
---
+++ script/galeria.js
@@ -0,0 +1,156 @@
+const apiConfig = [
+    { url: 'https://akabab.github.io/superhero-api/api/all.json', secao: '.exemplo1', titulo: 'Exemplo1'},
+    { url: 'https://valorant-api.com/v1/weapons?language=pt-BR', secao: '.exemplo2',  titulo: 'Exemplo2'},
+    { url: 'https://valorant-api.com/v1/maps?language=pt-BR', secao: '.exemplo3', titulo: 'Exemplo3'},
+    { url: 'https://valorant-api.com/v1/competitivetiers?language=pt-BR', secao: '.exemplo4', titulo: 'Exemplo4'}
+];
+function fetchData(url, secao){
+    fetch(url)
+    .then(response => response.json())
+    .then(dados => {
+        console.log(dados);
+        for (let i = 0; i < 25; i++) { // Pode ajustar o valor para a quantidade de cards que aparecem na rolagem
+            const image = dados[i].images.lg || dados[i].displayIcon;
+            const name = dados[i].name;
+            criarCards(image, name, secao);
+        }
+    })
+    .catch(error => {
+        console.error(`Error fetching data from ${url}:`, error);
+    });
+};
+
+
+apiConfig.forEach(item => {
+    criarSecoesCards(item.secao, item.titulo, item.url),
+    fetchData(item.url, item.secao)
+});
+
+function criarSecoesCards(secao, titulo, urlSecao){
+
+    const secaoConteudo = document.querySelector('.secaoConteudo');
+    const cardSecoes = document.createElement('section');
+    cardSecoes.className = `cardSecoes ${secao.substring(1)}`;
+    secaoConteudo.appendChild(cardSecoes);
+
+    const tituloSecao = document.createElement('div');
+    tituloSecao.className = 'tituloSecao';
+    cardSecoes.appendChild(tituloSecao);
+
+    const p = document.createElement('p');
+    p.innerHTML = titulo;
+    tituloSecao.appendChild(p);
+    
+    const verMais = document.createElement('div');
+    verMais.className = 'verMais';
+    
+    cardSecoes.appendChild(verMais);
+
+    const verMaisBotao = document.createElement('a');
+    verMaisBotao.className = 'verMaisBotao';
+    verMaisBotao.innerHTML = 'Ver mais &#10140;';
+    verMaisBotao.addEventListener('click', ()=> verMaisfunc(urlSecao, titulo))
+    
+    verMais.appendChild(verMaisBotao);
+
+    const botaoPrev = document.createElement('button');
+    botaoPrev.className = 'prev';
+    botaoPrev.innerHTML = '&#10094;';
+    botaoPrev.addEventListener('click', ()=> moveCarousel(secao, -1));
+    cardSecoes.appendChild(botaoPrev);
+
+    const botaoNext = document.createElement('button');
+    botaoNext.className = 'next';
+    botaoNext.innerHTML = '&#10095;';
+    botaoNext.addEventListener('click', () => moveCarousel(secao, 1));
+    cardSecoes.appendChild(botaoNext);
+
+    const cardContainer = document.createElement('div');
+    cardContainer.className = 'cardContainer';
+    cardSecoes.appendChild(cardContainer);
+}
+function criarCards(imgUrl, cardTitulo, cardSecao) {
+    const cardSecaoElement = document.querySelector(cardSecao);
+    if (!cardSecaoElement) {
+        console.error(`Elemento ${cardSecao} não encontrado.`);
+        return;
+    }
+
+    const cardContainer = cardSecaoElement.querySelector('.cardContainer');
+    if (!cardContainer) {
+        console.error(`Elemento .cardContainer não encontrado em ${cardSecao}.`);
+        return;
+    }
+
+    const a = document.createElement('a');
+    const card = document.createElement('div');
+    const cardImg = document.createElement('img');
+    const titulo = document.createElement('p');
+
+    a.className = 'cardLink';
+    a.addEventListener('click', () => criarPopup(imgUrl, cardTitulo));
+
+    card.className = 'card';
+
+    cardImg.className = 'cardImagem';
+    cardImg.src = imgUrl;
+
+    titulo.className = 'cardTexto';
+    titulo.innerHTML = cardTitulo;
+
+    card.appendChild(a);
+    card.appendChild(cardImg);
+    card.appendChild(titulo);
+    document.querySelector(cardSecao).querySelector('.cardContainer').appendChild(card);
+}
+function criarPopup(imgUrl, cardTitulo) {
+            let popupcontainer = document.querySelector('.popupcontainer');
+            if (!popupcontainer) {
+                popupcontainer = document.createElement('div');
+                popupcontainer.className = 'popupcontainer';
+                document.body.appendChild(popupcontainer);
+            }
+            const cardPopUp = document.createElement('div');
+            cardPopUp.className = 'cardPopUp';
+
+            const imgPopup = document.createElement('img');
+            imgPopup.className = 'imgPopup';
+            imgPopup.src = imgUrl;
+
+            const tituloPopup = document.createElement('h1');
+            tituloPopup.className = 'tituloPopup';
+            tituloPopup.innerHTML = cardTitulo;
+
+            const descricaoPopup = document.createElement('p');
+            descricaoPopup.className = 'descricaoPopup';
+            descricaoPopup.innerHTML = '';
+
+            const fecharPopup = document.createElement('button');
+            fecharPopup.className = 'fecharPopup';
+            fecharPopup.innerHTML = '&#10005;';
+            fecharPopup.addEventListener('click', abrirfecharPopUp);
+
+            cardPopUp.appendChild(imgPopup);
+            cardPopUp.appendChild(tituloPopup);
+            cardPopUp.appendChild(descricaoPopup);
+            cardPopUp.appendChild(fecharPopup);
+
+            popupcontainer.innerHTML = ''; // Limpar conteúdo anterior
+            popupcontainer.appendChild(cardPopUp);
+            setTimeout(() => {
+                abrirfecharPopUp()
+            }, 1);
+        }
+function abrirfecharPopUp(){
+    const popup = document.querySelector('.popupcontainer');
+    popup.classList.toggle('active');
+}
+function moveCarousel(secao, direcao) {
+    const container = document.querySelector(`${secao} .cardContainer`);
+    const scroll = container.clientWidth / 1; // Valor da rolagem horizontal do carrossel
+    container.scrollLeft += direcao * scroll;
+}
+function verMaisfunc(urlSecao, titulo){
+    window.location.href = 'galeriacompleta.html?url=' + urlSecao + '&titulo='+ titulo;
+
+}
